7319eac25fd676de96a6d702f06be057c44af3f6
MarkoB1 <marko.23b@gmail.com>
2016-11-02 17:04:43 +0100

    Contact V2

--- easy_cook/contact.php
+++ easy_cook/contact.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>TMPS00042</title>
+<title>Easy-Cook</title>
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 <link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
@@ -31,52 +31,26 @@
   <div class="panel">
       <div class="title">
         <h1>Contact</h1>
-       <h2>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.</h2>
+       <h2>Pour nous contacter, poser une question :</h2>
       </div>
       <div class="content">
         <form>
           <div class="contact-form mar-top30">
-            <label> <span>Full name</span>
+            <label> <span>Nom</span>
             <input type="text" class="input_text" name="name" id="name"/>
             </label>
             <label> <span>Email</span>
             <input type="text" class="input_text" name="email" id="email"/>
             </label>
-            <label> <span>Subject</span>
+            <label> <span>Objet</span>
             <input type="text" class="input_text" name="subject" id="subject"/>
             </label>
             <label> <span>Message</span>
             <textarea class="message" name="feedback" id="feedback"></textarea>
-            <input type="button" class="button" value="Submit " />
+            <input type="button" class="button" value="Valider" />
             </label>
           </div>
         </form>
-        <div class="address">
-          <div class="panel">
-            <div class="title">
-              <h1>Address - 01</h1>
-            </div>
-            <div class="content">
-              <p>Block No:5, Some where Road,<br />
-                City name, State, Country name</p>
-              <p class="padTop"><span>Phone :</span> 000 888 88888</p>
-              <p><span>Email :</span> <a href="mailto:info@companyname.com">info@companyname.com</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="address">
-        	<div class="panel ">
-            <div class="title">
-              <h1>Address - 02</h1>
-            </div>
-            <div class="content">
-              <p>Block No:5, Some where Road,<br />
-                City name, State, Country name</p>
-              <p class="padTop"><span>Phone :</span> 000 888 88888</p>
-              <p><span>Email :</span> <a href="mailto:info@companyname.com">info@companyname.com</a></p>
-            </div>
-          </div>
-        </div>
       </div>
       <div class="clear"></div>
     </div>
